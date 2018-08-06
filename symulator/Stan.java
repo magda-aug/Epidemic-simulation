e94a0746a077c15c858939ecfec634c0bb3450ff
Magdalena Augustyńska <32980499+magda-aug@users.noreply.github.com>
2018-08-06 23:01:48 +0200

    Create Stan.java

---
+++ symulator/Stan.java
@@ -0,0 +1,13 @@
+package symulator;
+
+/**
+ * Klasa reprezentująca możliwe stany zdrowia Agenta.
+ *
+ * @author Magdalena Augustyńska
+ * @version 2018.0611
+ */
+public enum Stan {
+    CHORY,
+    ZDROWY,
+    UODPORNIONY;
+}
